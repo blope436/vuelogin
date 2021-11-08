4a15124e2ef2476d5f942e9a5ec300856b1c4295
Brandon <blopez98@live.com>
2021-11-08 10:24:00 -0600

    Register Done for Real Login App

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,15 +4,30 @@ import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
 
 
-const { isAuthenticated, login } = useAuth();
+const { isAuthenticated, login, signup } = useAuth();
 
 const username = ref("");
 const password = ref("");
 
 const router = useRouter();
 
+const loggingIn = async () => {
+  await login(username.value, password.value);
+  goToHome();
+};
 
-import { useTimeout, promiseTimeout } from "@vueuse/core";
+const signingUp = async () => {
+  await signup(username.value, password.value);
+  goToHome();
+};
+
+const goToHome = () =>
+{
+    if (isAuthenticated.value)
+  {
+      router.push("/");
+  }
+};
 
 </script>
 
@@ -23,13 +38,16 @@ import { useTimeout, promiseTimeout } from "@vueuse/core";
 </div>
 <div class="flex flex-col justify-center text-center py-20 min-h-screen-nonav">
     <img class=" w-40 h-40 shadow-lg" src="/src/assets/password.png" alt="Login Image"/>
-    <form @submit.prevent="login(username,password)" class=" bg-indigo-300 flex flex-col space-y-10">
+    <form @submit.prevent="loggingIn" class=" bg-indigo-300 flex flex-col space-y-10">
         <input type="text" class="rounded-lg border-2" placeholder="Username" v-model="username"/>
         <input type="password" class="rounded-lg border-2 " placeholder="Password" v-model="password"/>
-        <button type="submit" @submit.prevent="login(username,password)" class="rounded-lg bg-blue-600 text-blue-200">Login</button>
+        <div class="flex justify-center space-x-3">
+            <button type="submit" @submit.prevent="loggingIn" class="py-2 px-4 rounded-lg bg-blue-600 text-blue-200">Login</button>
+            <button @click="signingUp" class=" py-2 px-4 rounded-lg bg-purple-600 text-red-200">Sign Up</button>
+        </div>
     </form>
 </div>
-<div class="absolute w-1/3 bg-pink-400 text-3xl bottom-2 right-2 rounded-lg p-4 text-center text-yellow-300">
+<div class="absolute w-1/3 bg-green-400 text-3xl bottom-2 right-2 rounded-lg p-4 text-center text-red-600">
     Logged In: {{isAuthenticated}}
 </div>
 
